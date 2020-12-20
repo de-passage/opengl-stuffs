4e637a3471619921b276914edd72cab4de7c99d1
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2020-12-20 18:19:11 +0800

    Finishes the buffer implementation

--- include/nuklear/buffer.hpp
+++ include/nuklear/buffer.hpp
@@ -29,36 +29,103 @@ struct fixed_size_buffer_allocator {
 
 template <class Alloc>
 class buffer : Alloc {
+  using Alloc::alloc;
+
  public:
-  template <class... Args>
+  template <class... Args,
+            std::enable_if_t<std::is_default_constructible_v<Alloc>, int> = 0>
   explicit buffer(Args&&... args) noexcept(
-      noexcept(Alloc::alloc(_buffer, std::forward<Args>(args)...))) {
-    Alloc::alloc(_buffer, std::forward<Args>(args)...);
+      noexcept(alloc(_buffer, std::forward<Args>(args)...))) {
+    alloc(_buffer, std::forward<Args>(args)...);
+  }
+
+  template <class A = Alloc,
+            class... Args,
+            std::enable_if_t<std::is_constructible_v<Alloc, A>, int> = 0>
+  explicit buffer(A&& alloc, Args&&... args) noexcept(
+      std::is_nothrow_constructible_v<Alloc, A>&& noexcept(
+          alloc(_buffer, std::forward<Args>(args)...)))
+      : Alloc{std::forward<A>(alloc)} {
+    alloc(_buffer, std::forward<Args>(args)...);
   }
 
   buffer(const buffer&) = delete;
-  buffer(buffer&& buf) noexcept
-      : _buffer{std::exchange(buf._buffer, nk_buffer{})} {}
+  buffer(buffer&& buf) noexcept(std::is_nothrow_move_constructible_v<Alloc>)
+      : Alloc(static_cast<Alloc&&>(buf)),
+        _buffer{std::exchange(buf._buffer, nk_buffer{})} {}
   buffer& operator=(const buffer&) = delete;
   buffer& operator=(buffer&& buf) noexcept {
     buffer{std::move(buf)}.swap(*this);
     return *this;
   }
 
-  inline void swap(buffer& buffer) noexcept {
-    nk_buffer = buffer._buffer;
-    buffer._buffer = _buffer;
-    _buffer = nk_buffer;
+  inline void swap(buffer& other) noexcept {
+    using std::swap;
+    std::swap(_buffer, other._buffer);
   }
 
   ~buffer() noexcept { Alloc::dealloc(_buffer); }
 
-  [[nodiscard]] const nk_buffer* buf() const noexcept { return _buffer; }
-  [[nodiscard]] nk_buffer* buf() noexcept { return &_buffer; }
+  [[nodiscard]] const nk_buffer& buf() const noexcept { return _buffer; }
+  [[nodiscard]] nk_buffer& buf() noexcept { return _buffer; }
 
-  void clear() noexcept { nk_buffer_clear(&_buffer); }
+  inline void clear() noexcept { nk_buffer_clear(&_buffer); }
+  inline void memory_info(nk_memory_status& info) noexcept {
+    nk_buffer_memory_info(&info, &_buffer);
+  }
+
+  [[nodiscard]] inline nk_memory_status memory_info() noexcept {
+    nk_memory_status st;
+    memory_info(st);
+    return st;
+  }
+
+  inline void push_front(const void* mem,
+                         nk_size size,
+                         nk_size align) noexcept {
+    _push(NK_BUFFER_FRONT, mem, size, align);
+  }
+
+  inline void push_back(const void* mem, nk_size size, nk_size align) noexcept {
+    _push(NK_BUFFER_BACK, mem, size, align);
+  }
+
+  inline void mark_front() noexcept { _mark(NK_BUFFER_FRONT); }
+
+  inline void mark_back() noexcept { _mark(NK_BUFFER_BACK); }
+
+  inline void reset_front() noexcept { _reset(NK_BUFFER_FRONT); }
+
+  inline void reset_back() noexcept { _reset(NK_BUFFER_BACK); }
+
+  [[nodiscard]] inline void* memory() noexcept {
+    return nk_buffer_memory(&_buffer);
+  }
+
+  [[nodiscard]] inline const void* memory() const noexcept {
+    return nk_buffer_memory_const(&_buffer);
+  }
+
+  [[nodiscard]] inline nk_size memory_total() noexcept {
+    return nk_buffer_memory_total(&_buffer);
+  }
 
  private:
+  inline void _push(nk_buffer_allocation_type type,
+                    const void* mem,
+                    nk_size size,
+                    nk_size align) noexcept {
+    nk_buffer_push(&_buffer, type, mem, size, align);
+  }
+
+  inline void _mark(nk_buffer_allocation_type type) noexcept {
+    nk_buffer_mark(&buffer, type);
+  }
+
+  inline void _reset(nk_buffer_allocation_type type) noexcept {
+    nk_buffer_reset(&buffer, type);
+  }
+
   nk_buffer _buffer;  // NOLINT
 };
 
